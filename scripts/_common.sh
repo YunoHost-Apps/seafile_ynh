734b6d12d96f8ef120bfb1a275cbedede2bac575
Josué Tille <josue@tille.ch>
2024-12-16 12:31:35 +0100

    First draft upgrade to seafile 12

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,10 @@ source /usr/share/yunohost/helpers
 #=================================================
 
 readonly seafile_version=$(ynh_app_upstream_version)
-readonly seafile_code="$install_dir/seafile_image/opt/seafile/seafile-server-$seafile_version"
+
+readonly seafile_image="$install_dir/seafile_image"
+readonly notification_image="$install_dir/notification_image"
+readonly seafile_code="$seafile_image/opt/seafile/seafile-server-$seafile_version"
 
 readonly time_zone="$(cat /etc/timezone)"
 readonly python_version="$(python3 -V | cut -d' ' -f2 | cut -d. -f1-2)"
@@ -16,6 +19,11 @@ systemd_seafile_bind_mount+="$install_dir/conf:/opt/seafile/conf "
 systemd_seafile_bind_mount+="$install_dir/ccnet:/opt/seafile/ccnet "
 systemd_seafile_bind_mount+="/proc "
 systemd_seafile_bind_mount+="/dev"
+
+systemd_notification_server_bind_mount="$data_dir/notification-data:/opt/notification-data "
+systemd_notification_server_bind_mount+="$app-notification-server:/opt/seafile/logs "
+systemd_notification_server_bind_mount+="/proc "
+systemd_notification_server_bind_mount+="/dev"
 
 # Create special path with / at the end
 if [[ "$path" == '/' ]]
@@ -39,14 +47,21 @@ run_seafile_cmd() {
     ynh_hide_warnings systemd-run --wait --uid="$app" --gid="$app" \
         --property=RootDirectory="$install_dir"/seafile_image \
         --property="BindPaths=$systemd_seafile_bind_mount" \
-        $@
+        --property=EnvironmentFile="$install_dir"/seafile_env.conf \
+        "$@"
 }
 
 install_source() {
     # set correct seafile version in patch
     ynh_replace --match="__SEAFILE_VERSION__" --replace="$seafile_version" --file="$YNH_APP_BASEDIR"/patches/main/import_ldap_user_when_authenticated_from_remoteUserBackend.patch
-    ynh_setup_source_custom --dest_dir="$install_dir"/seafile_image --full_replace
+    ynh_setup_source_custom --dest_dir="$seafile_image" --full_replace
     mkdir -p "$install_dir"/seafile_image/opt/seafile/{seafile-data,seahub-data,conf,ccnet,logs}
+    grep "^$app:x"  /etc/passwd | sed "s|$install_dir|/opt/seafile|" >> "$install_dir"/seafile_image/etc/passwd
+    grep "^$app:x"  /etc/group >> "$install_dir"/seafile_image/etc/group
+    grep "^$app:x"  /etc/group- >> "$install_dir"/seafile_image/etc/group-
+    grep "^$app:"  /etc/shadow >> "$install_dir"/seafile_image/etc/shadow
+
+    ynh_setup_source_custom --dest_dir="$notification_image" --full_replace --source_id=notification_server
     grep "^$app:x"  /etc/passwd | sed "s|$install_dir|/opt/seafile|" >> "$install_dir"/seafile_image/etc/passwd
     grep "^$app:x"  /etc/group >> "$install_dir"/seafile_image/etc/group
     grep "^$app:x"  /etc/group- >> "$install_dir"/seafile_image/etc/group-
@@ -60,6 +75,8 @@ set_permission() {
     chmod -R u+rwX,g+rX-w,o= "$install_dir"/{conf,ccnet}
     chown -R "$app:$app" "$install_dir"/seafile_image/opt/seafile
     chmod -R u+rwX,g-w,o= "$install_dir"/seafile_image/opt/seafile
+    chown -R "$app:$app" /var/log/{"$app","$app"-notification-server}
+    chmod -R u=rwX,g=rX,o= /var/log/{"$app","$app"-notification-server}
 
     # Allow to www-data to each dir between /opt/yunohost/seafile and /opt/yunohost/seafile/seafile_image/opt/seafile/seahub/media
     local dir_path=''
@@ -92,6 +109,6 @@ clean_url_in_db_config() {
 }
 
 ensure_vars_set() {
-    ynh_app_setting_set_default --key=jwt_private_key_notification_server --value=$(ynh_string_random -l 32)
+    ynh_app_setting_set_default --key=jwt_private --value=$(ynh_string_random -l 32)
     ynh_app_setting_set_default --key=protect_against_basic_auth_spoofing --value=false
 }
